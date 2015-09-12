4ddecb27fcaf70fef96c9809739da965f3bfe99f
tinpascual <tin.pascual@gmail.com>
2015-09-12 23:13:06 +0800

    plot1 R and png added

---
+++ plot1.R
@@ -0,0 +1,12 @@
+plot1<-function(){
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "powerData.zip")
+    unzip("powerData.zip")
+    dataColNames = read.table("household_power_consumption.txt", nrows=1, sep=";", stringsAsFactors = FALSE)
+    powerData = read.table(pipe('grep "^[1-2]/2/2007" "household_power_consumption.txt"'), sep=";", col.names=dataColNames[1, ])
+    
+    # plot 1: Global_active_power histogram
+    plot1<-hist(powerData$Global_active_power)
+    png(filename = "plot1.png", width = 480, height = 480)
+    plot(plot1, col="red1", xlab="Global Active Power (kilowatts)", ylab="Frequency", main="Global Active Power")
+    dev.off()
+}
